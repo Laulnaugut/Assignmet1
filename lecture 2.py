3774c8fbe2d0be604b092bfcc6df5bec67ba3c18
Laulnaugut <148615523+Laulnaugut@users.noreply.github.com>
2025-04-13 19:24:55 +0530

    Add files via upload

---
+++ lecture 2.py
@@ -0,0 +1,7 @@
+num1 = float(input( "Enter first number: "))
+num2 = float(input( "Enter second number: "))
+sum = num1+num2
+print("the sum of all the numbers is:" ,sum )
+print("the subrtraction is:",(num1-num2)) 
+print("the product of both the numbers is:",(num1*num2))
+print("the division of both the numbers is:", (num1/num2))
